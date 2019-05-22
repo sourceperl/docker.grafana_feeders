20ef715e2ddce1772b4f09b34adb34c4ed487556
sourceperl <loic.celine@free.fr>
2019-05-22 12:26:18 +0200

    fix check stability

--- check-pid-learning-stability/check-pid-learning.py
+++ check-pid-learning-stability/check-pid-learning.py
@@ -6,13 +6,15 @@ import numpy as np
 from scipy import fftpack
 from influxdb import InfluxDBClient
 
+# some const
+TS = 2  # second between 2 samples
 
 # connect to influxdb DB
 client = InfluxDBClient(host="influxdb", port=8086)
 client.switch_database("mydb")
 
-# request for select last 3 records of "field1" in "test" measurement (mean value by 10s steps)
-req = "SELECT mean(\"pv\") AS f1 FROM \"maquette_pid\" GROUP BY time(10s) fill(null) ORDER BY time DESC LIMIT 400"
+# select last value (to avoid null first value we must do time(xxs, now()) and no time(xxs))
+req = "SELECT mean(\"out\") AS f1 FROM \"maquette_pid\" WHERE time < now() - 4s GROUP BY time(%ds, now()) fill(previous) ORDER BY time DESC LIMIT 150" % TS
 
 # format data and check all data is available
 l_points = []
@@ -25,12 +27,9 @@ for point in client.query(req).get_points():
 # number of samples
 Ns = len(l_points)
 
-# second between 2 samples
-Ts = 10  # 10 s
-
 # N samples, every sample is time value (in s)
 # 0 -> t_max with Ns items
-t_max = Ts * Ns
+t_max = TS * Ns
 t_samples = np.linspace(0.0, t_max, Ns)
 
 # convert list to numpy array
@@ -38,11 +37,11 @@ y_samples = np.asarray(l_points)
 
 # build signal
 nb = len(y_samples)
-x = np.linspace(0.0, (nb - 1) * Ts, nb)
+x = np.linspace(0.0, (nb - 1) * TS, nb)
 
 # compute fft
 yf = fftpack.fft(y_samples)
-xf = np.linspace(0.0, 1.0 / (2.0 * Ts), nb // 2)
+xf = np.linspace(0.0, 1.0 / (2.0 * TS), nb // 2)
 ya = 2.0 / nb * np.abs(yf[:nb // 2])
 
 # normalize ya to % of signal
